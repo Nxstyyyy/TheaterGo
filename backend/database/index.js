edefb1edb7ddd0964e31576971ffd1b94bad534e
Nxsty <gianniskolliniatis@outlook.com>
2026-05-04 02:55:34 +0300

    third attemp to fix vercel

--- backend/database/index.js
+++ backend/database/index.js
@@ -7,6 +7,7 @@ async function getPool() {
         pool = createPool({
             host: process.env.DB_HOST || 'localhost',
             user: process.env.DB_USER || 'root',
+            port: process.env.DB_PORT || 3306,
             password: process.env.DB_PASSWORD || '',
             database: process.env.DB_NAME || 'myapp',
             connectionLimit: 5
